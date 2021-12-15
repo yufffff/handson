b914352c5070eeb8f98244a63267c750ef1a33d5
yufffff <y.fukai.0325@gmail.com>
2021-12-15 18:07:55 +0900

    local commit

--- App.js
+++ App.js
@@ -1,21 +1,145 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, TextInput, FlatList } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+function Input(props) {
+  const [text, setText] = useState('')
+  const onPress = () => {
+    props.addEet(text)
+    setText('')
+  }
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.input} onChangeText={(_text) => setText(_text)} />
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text style={styles.buttonText}>イートする</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+function Eet(props) {
+  const {
+    text,
+    like,
+    onLike,
+  } = props
+  return (
+    <View style={eetStyles.container}>
+      <Text style={eetStyles.text}>{text}</Text>
+      <View style={eetStyles.actionContainer}>
+        <TouchableOpacity onPress={onLike}>
+          {like ?
+            <Ionicons name="heart-circle-sharp" size={22} color="rgb(252, 108, 133)"/> :
+            <Ionicons name="ios-heart-circle-outline" size={22} color="#aaa"/>
+          }
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
 
 export default function App() {
+  const [eet, setEet] = useState([])
+  const addEet = (text) => {
+    const newEet = [].concat(eet)
+    newEet.push({
+      text,
+      id: Date.now(),
+    })
+    setEet(newEet)
+  }
+  const onLike = (index) => {
+    const newEet = [].concat(eet)
+    newEet[index].like = !newEet[index].like
+    setEet(newEet)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Input addEet={addEet}/>
+        <View style={styles.content}>
+          <FlatList
+            data={eet}
+            renderItem={({item, index}) => <Eet text={item.text} like={item.like} onLike={() => onLike(index)} />}
+            keyExtractor={(item) => `${item.id}`}
+            contentContainerStyle={styles.contentContainer}
+          />
+        </View>
+        <StatusBar style="light" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#222',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 20,
+  },
+  button: {
+    backgroundColor: 'rgb(29, 161, 242)',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderRadius: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '900',
+    fontSize: 16,
+  }, 
+  inputContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+  },
+  input: {
+    flex: 1,
+    borderColor: 'rgb(29, 161, 242)',
+    borderWidth: 2,
+    marginRight: 10,
+    borderRadius: 10,
+    color: 'white',
+    paddingHorizontal: 10,
+    fontSize: 16,
+  },
+  content: {
+    padding: 20,
+    flex: 1,
+  },
+  contentText: {
+    color: 'white',
+    fontSize: 22,
+  },
+  contentContainer: {
+    paddingBottom: 50,
   },
 });
+
+const eetStyles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderColor: 'rgb(29, 161, 242)',
+    marginBottom: 10,
+    borderRadius: 5,
+  },
+  text: {
+    color: 'white',
+    fontSize: 16,
+  },
+  actionContainer: {
+    borderTopWidth: 1,
+    borderTopColor: '#aaa',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    paddingTop: 5,
+    marginTop: 20,
+  }
+})
